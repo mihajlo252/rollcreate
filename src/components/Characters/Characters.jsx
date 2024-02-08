a8e5f4f1274188443142f2d84577035125f85c7e
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-02-08 16:09:16 +0100

    Refactor

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { CharacterCard } from "../CharacterCard/CharacterCard";
-import { getAllUserCharacters } from "../../utilities/getAllUserCharacters";
+import { getAllUserTables } from "../../utilities/getAllUserTables";
 import CharactersStyles from "./CharactersStyles.module.css";
 // getting all characters
 // import { getAllData } from "../utilities/getAllData";
@@ -15,7 +15,7 @@ export const Characters = ({ userData }) => {
 	// };
 
 	const handleGetAllUserCharacters = async () => {
-		const res = await getAllUserCharacters(userData.user.id);
+		const res = await getAllUserTables("characters", userData.user.id);
 		setCharacters(res.data);
 	};
 
@@ -30,7 +30,7 @@ export const Characters = ({ userData }) => {
 		<div
 			className={`flex w-[50%] flex-col justify-around rounded-[15px] bg-[#0e0d14] bg-opacity-70 px-5 py-9`}
 		>
-			<h2 className="text-4xl text-neutral">Characters</h2>
+			<h2 className="text-5xl text-neutral">Characters</h2>
 			<div className={`overflow-y-scroll ${CharactersStyles.scrollable}`}>
 				<div className="flex h-[500px] flex-col gap-2 pe-2">
 					{characters.map((character) => (
