0e90b857ac26ee6616895b7cfa87958a162bc79a
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-02-08 16:07:59 +0100

    Refactor

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -9,24 +9,24 @@ export const CharacterCard = ({ character }) => {
 		<NavLink
 			to={`./characters/${character.id}`}
 			state={meta_data}
-			className="hover:scale-[1.01] transition-transform"
+			className="transition-transform hover:scale-[1.01]"
 		>
 			<div
-				className={`bg-[#0e0d14] bg-opacity-80 ps-10 pe-5 py-5 items-center justify-center relative`}
+				className={`relative items-center justify-center bg-[#0e0d14] py-5 pe-5 ps-10`}
 			>
 				<div className={`${CardStyles.card}`}>
 					<h1 className={`text-3xl text-neutral self-center ${CardStyles.char_name}`}>
 						{meta_data.name}
 					</h1>
 					<div className={`${CardStyles.main_traits}`}>
-						<div className="grid grid-cols-3 gap-2 min-h-6 place-items-center px-1 text-sm">
+						<div className="min-h-6 grid grid-cols-3 place-items-center gap-2 px-1 text-sm">
 							<p className="place-self-start self-center">Race</p>
 							<p>Class</p>
 							<p className="place-self-end self-center">Affiliation</p>
 							<p className="place-self-start self-center text-neutral-400">
 								{meta_data.race.main}, {meta_data.race.sub}
 							</p>
-							<p className=" text-neutral-400">{meta_data.class.main}</p>
+							<p className="text-neutral-400">{meta_data.class.main}</p>
 							<p className="place-self-end self-center text-neutral-400">
 								{meta_data.affiliation}
 							</p>
