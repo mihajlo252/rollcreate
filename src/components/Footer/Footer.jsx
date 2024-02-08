d194681846c0827d662ca8f243e4016c894ea878
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-02-08 16:08:59 +0100

    Add footer image

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BsGithub } from "react-icons/bs";
 
+import FooterStyles from "./FooterStyles.module.css";
+
 export const Footer = () => {
 	return (
 		<footer
-			className={`text-white-content footer footer-center bg-[#0e0d14] bg-opacity-80 p-8 min-[600px]:backdrop-blur-sm`}
+			className={`text-white-content footer footer-center p-8 ${FooterStyles.footer}`}
 		>
 			<div className="flex flex-col gap-3">
 				<a href="https://github.com/mihajlo252" aria-label="Github Link">
