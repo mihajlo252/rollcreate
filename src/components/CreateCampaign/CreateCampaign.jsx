9b0ff2024e1ff2d542a2f0f421e1ea0e47660838
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-02-14 18:30:03 +0100

    Add CreateCampaign and CreateCharacter component

---
+++ src/components/CreateCampaign/CreateCampaign.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateCampaign = () => {
+  return (
+    <div>CreateCampaign</div>
+  )
+}
---
+++ src/components/CreateCharacter/CreateCharacter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateCharacter = () => {
+  return (
+    <div>CreateCharacter</div>
+  )
+}
