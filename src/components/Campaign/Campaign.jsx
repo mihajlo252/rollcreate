cfd2d0afc554fdfb980c0cf11a2fd8b2c87f4b35
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-03-01 16:02:47 +0100

    Add map lazy load, skeleton animation and location state data transfer

--- src/components/Campaign/Campaign.jsx
+++ src/components/Campaign/Campaign.jsx
@@ -1,34 +1,43 @@
 import { motion } from "framer-motion";
-import React, { useEffect, useState } from "react";
-import { Form } from "react-router-dom";
+import React, { lazy, useEffect, useState } from "react";
+import { Form, useLocation } from "react-router-dom";
 import { supabase } from "../../supabase/supabase";
 
-
 export const Campaign = () => {
-	const [imageUrl, setImageUrl] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
 
-	const getImageFromStorage = async () => {
-		const { data } = supabase.storage.from("maps").getPublicUrl("map.png");
-		setImageUrl(data.publicUrl);
-	};
+  const {state} = useLocation()
+
+  const getImageFromStorage = async () => {
+    const { data } = supabase.storage.from("maps").getPublicUrl(state.meta_data.map);
+    setImageUrl(data.publicUrl);
+  };
 
   useEffect(() => {
     getImageFromStorage();
   }, []);
 
-
-	return (
-		<motion.section
-			className="grid w-full grid-cols-[2fr_1fr]"
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-		>
-			<div className="flex h-full w-full items-center justify-center bg-black">
-				<img src={imageUrl} alt="map" className="h-full w-full" />
-			</div>
-			<aside className="flex flex-col items-center bg-black bg-opacity-60 px-5 py-10">
-				<h2 className="text-4xl text-primary">Campaign Name</h2>
-			</aside>
-		</motion.section>
-	);
+  return (
+    <motion.section
+      className="grid w-full grid-cols-[2fr_1fr] bg-black bg-opacity-60"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <div className="flex h-full w-full relative items-center justify-center ">
+        {!isLoaded && <div className={`w-full h-full bg-[#0d0d0d] animate-pulse opacity-35 ${isLoaded && "hidden"}`}></div> }
+          <img
+            src={imageUrl}
+            alt="map"
+            className={`h-full w-full absolute opacity-1 transition-all duration-[250ms] ${!isLoaded && "opacity-0"}`}
+            onLoad={() => setIsLoaded(true)}
+            loading="lazy"
+          />
+        
+      </div>
+      <aside className="flex flex-col items-center px-5 py-10">
+        <h2 className="text-4xl text-primary">{state.campaign_name}</h2>
+      </aside>
+    </motion.section>
+  );
 };
--- src/components/CampaignCard/CampaignCard.jsx
+++ src/components/CampaignCard/CampaignCard.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import CardStyles from "./CampaignCardStyles.module.css";
+import { NavLink } from "react-router-dom";
 
 export const CampaignCard = ({ campaign }) => {
-	const { campaign_name, meta_data, featured, created_at } = campaign;
+  const { campaign_name, meta_data, featured, created_at } = campaign;
 
-	return (
-		<div className={`flex flex-col px-5 py-2 min-h-[15rem] bg-neutral-content bg-opacity-70 rounded-md backdrop-blur ${CardStyles.card}`}>
-			<h3 className="text-2xl text-primary">{campaign_name}</h3>
-			<p>{meta_data.type}</p>
-			<p>{meta_data.players}</p>
-			<p>{featured ? "Featured" : "Not featured"}</p>
-			<p>{created_at}</p>
-		</div>
-	);
+  return (
+    <NavLink to={`./${campaign.id}`} state={campaign}>
+      <div
+        className={`flex flex-col px-5 py-2 min-h-[15rem] bg-neutral-content bg-opacity-70 rounded-md backdrop-blur ${CardStyles.card}`}
+      >
+        <h3 className="text-2xl text-primary">{campaign_name}</h3>
+        <p>{meta_data.type}</p>
+        <p>{meta_data.players}</p>
+        <p>{featured ? "Featured" : "Not featured"}</p>
+        <p>{created_at}</p>
+      </div>
+    </NavLink>
+  );
 };
