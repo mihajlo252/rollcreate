34cb8381365c5afbff82946978263cdbe6b91f7d
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-03-09 00:04:58 +0100

    Add handleCreateCampaign

--- src/components/CreateCampaign/CreateCampaign.jsx
+++ src/components/CreateCampaign/CreateCampaign.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { Form } from "react-router-dom";
+import { Form, useNavigate } from "react-router-dom";
+import { submitCampaign } from "../../utilities/submitCampaign";
+import { useSelector } from "react-redux";
 
 export const CreateCampaign = () => {
   const [name, setName] = useState("");
-  const [type, setType] = useState("");
+  const [map, setMap] = useState()
+
+  const { userData } = useSelector((state) => state.userData);
+  const navigate = useNavigate()
+
+
+  // const handleNavigate = async (data) => {
+  //   const reroute = `/campaigns/${data.dbData[0].id}`
+  //   navigate(reroute)
+  // }
+
+  const handleCreateCampaign = async (e) => {
+    e.preventDefault()
+    const data = await submitCampaign(userData.user.id, name, map);
+    console.log(data);
+    // await handleNavigate(data)
+  };
 
   return (
     <motion.section
@@ -13,19 +31,16 @@ export const CreateCampaign = () => {
       animate={{ opacity: 1 }}
     >
       <h2 className="text-5xl text-primary">Create Campaign</h2>
-      <Form className="flex flex-col gap-5">
+      <Form className="flex flex-col gap-5" onSubmit={handleCreateCampaign}>
         <div className="flex flex-col gap-2">
           <label htmlFor="name">Name your campaign:</label>
           <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Indispensables" />
         </div>
         <div className="flex flex-col gap-2">
-          <label htmlFor="type">Type:</label>
-          <input type="text" id="type" value={type} onChange={(e) => setType(e.target.value)} placeholder="Oneshot" />
-        </div>
-        <div className="flex flex-col gap-2">
             <label htmlFor="map">Upload your map:</label>
-            <input type="file" />
+            <input type="file" onChange={(e) => setMap(e.target.files[0])}/>
         </div>
+        <button className="btn btn-primary">Create</button>
       </Form>
     </motion.section>
   );
--- src/utilities/submitCampaign.jsx
+++ src/utilities/submitCampaign.jsx
@@ -1,12 +1,11 @@
 import { supabase } from "../supabase/supabase";
 
-export async function submitCampaign(metaData) {
-  const { data, error } = await supabase
-    .from('campaigns')
-    .insert([
-      {}
-    ])
-    .select()
-            
-  return {data: data, error: error}
+export async function submitCampaign(profileId, campaignName, mapFile) {
+	const { data: dbData, error: dbError } = await supabase
+		.from("campaigns")
+		.insert([{profile: profileId,  meta_data: { map: mapFile.name }, campaign_name: campaignName }])
+		.select();
+	const { data: storageData, error: storageError } = await supabase.storage.from("maps").upload(mapFile.name, mapFile);
+
+	return { dbData, dbError, storageData, storageError };
 }
