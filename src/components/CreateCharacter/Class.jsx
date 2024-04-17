179b45959aa68f24e305d3c8f18ad99f1dc368cd
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-04-17 21:59:12 +0200

    Add Class details

--- src/components/CreateCharacter/Class.jsx
+++ src/components/CreateCharacter/Class.jsx
@@ -5,9 +5,12 @@ export const Class = ({ classes }) => {
 	const [selectedClass, setSelectedClass] = useState("Barbarian");
 	const [selectedClassData, setSelectedClassData] = useState({});
 
+	
+
 	useEffect(() => {
 		setSelectedClassData(classes.find((item) => item.name === selectedClass));
 	}, [selectedClass]);
+
 	return (
 		<motion.section
 			className="flex h-full w-full items-center justify-center gap-10"
@@ -28,8 +31,20 @@ export const Class = ({ classes }) => {
 					))}
 				</select>
 			</div>
-			<aside className="flex h-full w-1/2 flex-col items-center justify-center rounded-[15px] bg-[#09080D] bg-opacity-70 px-5 py-9 text-sm">
-				<p>{selectedClassData.description}</p>
+			<aside className="flex h-full w-1/2 flex-col items-center overflow-y-scroll rounded-[15px] bg-[#09080D] bg-opacity-70 px-5 py-9 text-sm">
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					key={selectedClass}
+					className="h-[200px] w-full"
+				>
+					<p className="mb-2">{selectedClassData.description}</p>
+					<p className="mb-1">Hit Dice: {selectedClassData.class_features?.hit_dice}</p>
+					<p className="mb-1">Armor: {selectedClassData.class_features?.proficiencies.armor}</p>
+					<p className="mb-1">Weapons: {selectedClassData.class_features?.proficiencies.weapons}</p>
+					<p className="mb-1">Tools: {selectedClassData.class_features?.proficiencies.tools}</p>
+				</motion.div>
 			</aside>
 		</motion.section>
 	);
