018a553c8236a56eb8e1534cb238490b45d568c7
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-04-27 22:29:13 +0200

    Add profileId instead of placeholder

--- src/utilities/submitCharacter.jsx
+++ src/utilities/submitCharacter.jsx
@@ -4,7 +4,7 @@ export async function submitCharacter(metaData, profileId, campaignId) {
   const { data, error } = await supabase
     .from('characters')
     .insert([
-      {meta_data: metaData, profile: "d9d5da45-16f6-43a0-b18b-14b60dbdf508", campaign: campaignId}
+      {meta_data: metaData, profile: profileId, campaign: campaignId}
     ])
     .select()
             
