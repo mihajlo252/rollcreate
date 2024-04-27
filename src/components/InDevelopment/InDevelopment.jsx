55521dbfc65da5438a139c7ad117fe7d45efe4b7
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-04-27 22:50:50 +0200

    Add InDevelopment page

---
+++ src/components/InDevelopment/InDevelopment.jsx
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+export const InDevelopment = () => {
+    return (
+        <motion.section
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex w-full flex-col items-center justify-center gap-10 rounded-[15px] bg-[#09080D] bg-opacity-60 px-20 py-9"
+        >
+            <h1 className="text-5xl text-primary">This page is currently in <span>development</span>.</h1> 
+            <h2 className="text-3xl text-primary"> Please check out our other content that is available and <span className="text-secondary">stay tuned!</span></h2>
+            {/* <p className="text-xl text-secondary">Stay tuned!</p> */}
+        </motion.section>
+    );
+};
--- src/routes/Create.jsx
+++ src/routes/Create.jsx
@@ -4,35 +4,38 @@ import { motion } from "framer-motion";
 import { pageChange } from "../redux/page";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { AnimatedCard } from "../components/AnimatedCard/AnimatedCard";
+import { InDevelopment } from "../components/InDevelopment/InDevelopment";
+const development = import.meta.env.VITE_DEVELOPMENT_MODE || false;
 
 export const Create = () => {
-	const { dispatch } = useOutletContext();
+    const { dispatch } = useOutletContext();
 
-	const navigate = useNavigate()
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        dispatch(pageChange("create"));
+    }, []);
 
-	useEffect(() => {
-		dispatch(pageChange("create"));
-	}, []);
+	if(!development) return <InDevelopment />
 
-	return (
-		<motion.section
-			className="flex w-full items-center justify-around gap-10"
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-		>
-			<button onClick={() => navigate("./character")}>
-				<AnimatedCard
-					text="Create a character"
-					bg="assets/images/background-soldier-portrait-sm.png"
-				/>
-			</button>
-			<button onClick={() => navigate("./campaign")}>
-				<AnimatedCard
-					text="Create a campaign"
-					bg="assets/images/background-soldier-portrait-sm.png"
-				/>
-			</button>
-		</motion.section>
-	);
+    return (
+        <motion.section
+            className="flex w-full items-center justify-around gap-10"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+        >
+            <button onClick={() => navigate("./character")}>
+                <AnimatedCard
+                    text="Create a character"
+                    bg="assets/images/background-soldier-portrait-sm.png"
+                />
+            </button>
+            <button onClick={() => navigate("./campaign")}>
+                <AnimatedCard
+                    text="Create a campaign"
+                    bg="assets/images/background-soldier-portrait-sm.png"
+                />
+            </button>
+        </motion.section>
+    );
 };
