244cab415f02101f3f1f5e186f6baaa0e2fb1bf9
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-05-03 17:50:59 +0200

    Add CreateCard clickable indicator

--- src/components/CreateCard/CreateCard.jsx
+++ src/components/CreateCard/CreateCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CreateCardStyles from "./CreateCardStyles.module.css";
 
@@ -6,7 +6,7 @@ export const CreateCard = ({ children, to }) => {
     return (
         <Link
             to={to}
-            className={`card w-full place-items-center gap-20 bg-[#09080D] bg-opacity-70 transition-transform hover:scale-[1.05] ${CreateCardStyles.card}`}
+            className={`card w-full place-items-center gap-20 bg-[#09080D] bg-opacity-70 transition-transform hover:scale-[1.05] animate-pulse animate-delay-1000 animate-once animate-alternate animate-duration-1000 animate-ease-in-out ${CreateCardStyles.card}`}
         >
             {children}
         </Link>
