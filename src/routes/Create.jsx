c406df5167ae844afd520e334d49f4eb507a3756
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-05-03 13:43:21 +0200

    Remove AnimatedCard and refactor

--- src/routes/Create.jsx
+++ src/routes/Create.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { getDndData } from "../utilities/getDndData";
 import { motion } from "framer-motion";
 import { pageChange } from "../redux/page";
-import { useNavigate, useOutletContext } from "react-router-dom";
-import { AnimatedCard } from "../components/AnimatedCard/AnimatedCard";
+import { Link, useNavigate, useOutletContext } from "react-router-dom";
 import { InDevelopment } from "../components/InDevelopment/InDevelopment";
 
 export const Create = () => {
-	const development = false
+    const development = false;
     const { dispatch } = useOutletContext();
 
     const navigate = useNavigate();
@@ -16,26 +15,36 @@ export const Create = () => {
         dispatch(pageChange("create"));
     }, []);
 
-	if(development) return <InDevelopment />
+    if (development) return <InDevelopment />;
 
     return (
         <motion.section
-            className="flex w-full items-center justify-around gap-10"
+            className="flex w-full items-center justify-center"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
         >
-            <button onClick={() => navigate("./character")}>
-                <AnimatedCard
-                    text="Create a character"
-                    bg="assets/images/background-soldier-portrait-sm.png"
-                />
-            </button>
-            <button onClick={() => navigate("./campaign")}>
-                <AnimatedCard
-                    text="Create a campaign"
-                    bg="assets/images/background-soldier-portrait-sm.png"
-                />
-            </button>
+            <div className="grid h-min w-full grid-cols-2 gap-10">
+                <Link to="./character" className="card place-items-center gap-20 bg-[#09080D] bg-opacity-70 px-20 py-10">
+                    <h2 className="place-self-center text-5xl text-primary">
+                        Create your character!
+                    </h2>
+                    <p className="text-2xl text-neutral">
+                        Here you will be able to choose between a plethora of
+                        customization options. Want to be a heroic knight or an
+                        evil mage? You got it! If you think it, you can probably
+                        build it. The world is your oyster!
+                    </p>
+                </Link>
+                <Link to="./campaign" className="card place-items-center gap-20 bg-[#09080D] bg-opacity-70 px-20 py-10">
+                    <h2 className="place-self-center text-5xl text-primary">
+                        Create your campaign!
+                    </h2>
+                    <p className="text-2xl text-neutral">
+                        Make your dream world come to life. Have fun building
+                        and exploring your ideas with us!
+                    </p>
+                </Link>
+            </div>
         </motion.section>
     );
 };
