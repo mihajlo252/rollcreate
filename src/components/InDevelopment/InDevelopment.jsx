38cf0761e4111fd34dec5340d235793e189fb694
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-05-04 15:02:48 +0200

    Refactor and publish

--- src/components/InDevelopment/InDevelopment.jsx
+++ src/components/InDevelopment/InDevelopment.jsx
@@ -9,8 +9,8 @@ export const InDevelopment = () => {
             exit={{ opacity: 0 }}
             className="flex w-full flex-col items-center justify-center gap-10 rounded-[15px] bg-[#09080D] bg-opacity-60 px-20 py-9"
         >
-            <h1 className="text-5xl text-primary">This page is currently in <span>development</span>.</h1> 
-            <h2 className="text-3xl text-primary"> Please check out our other content that is available and <span className="text-secondary">stay tuned!</span></h2>
+            <h1 className="text-5xl text-warning">This page is currently in <span>development</span>.</h1> 
+            <h2 className="text-3xl text-warning"> Please check out our other content that is available and <span className="text-secondary">stay tuned!</span></h2>
         </motion.section>
     );
 };
--- src/routes/Create.jsx
+++ src/routes/Create.jsx
@@ -2,19 +2,24 @@ import React, { useEffect } from "react";
 import { getDndData } from "../utilities/getDndData";
 import { motion } from "framer-motion";
 import { pageChange } from "../redux/page";
-import { useOutletContext } from "react-router-dom";
-import { InDevelopment } from "../components/InDevelopment/InDevelopment";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import { CreateCard } from "../components/CreateCard/CreateCard";
+import { useSelector } from "react-redux";
 
 export const Create = () => {
-    const development = false;
     const { dispatch } = useOutletContext();
+    const { userData } = useSelector((state) => state.userData);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
+        if (!userData) {
+            navigate("/signin");
+        }
         dispatch(pageChange("create"));
     }, []);
 
-    if (development) return <InDevelopment />;
+    if (!userData) return <></>;
 
     return (
         <motion.section
@@ -22,18 +27,10 @@ export const Create = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
         >
-            <h1 className="text-center text-[2.8rem] text-primary">Choose what you want to create</h1>
+            <h1 className="text-center text-[2.8rem] text-primary">
+                Choose what you want to create
+            </h1>
             <div className="grid h-min w-full gap-10 max-[1000px]:grid-rows-2 min-[1000px]:grid-cols-2">
-                <CreateCard to="./character">
-                    <h2 className="place-self-center text-center text-5xl text-primary">
-                        Create your character
-                    </h2>
-                    <p className="text-center text-neutral">
-                        Want to be a heroic knight or an
-                        evil warlock? You got it! If you think it, you can probably
-                        build it. The world is your oyster!
-                    </p>
-                </CreateCard>
                 <CreateCard to="./campaign">
                     <h2 className="place-self-center text-center text-5xl text-primary">
                         Create your campaign
@@ -41,6 +38,16 @@ export const Create = () => {
                     <p className="text-center text-neutral">
                         Make your dream world come to life. Have fun building
                         and exploring your ideas with us!
+                    </p>
+                </CreateCard>
+                <CreateCard to="./character" inDevelopment={true}>
+                    <h2 className="place-self-center text-center text-5xl text-primary">
+                        Create your character
+                    </h2>
+                    <p className="text-center text-neutral">
+                        Want to be a heroic knight or an evil warlock? You got
+                        it! If you think it, you can probably build it. The
+                        world is your oyster!
                     </p>
                 </CreateCard>
             </div>
