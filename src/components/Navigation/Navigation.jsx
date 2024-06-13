29cbe3006795a107de96584d1032325db7eaa51c
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-06-13 17:13:23 +0200

    Refactor

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -9,56 +9,61 @@ import { MenuAndProfileGroup } from "../MenuAndProfileGroup/MenuAndProfileGroup"
 import NavigationStyles from "./NavigationStyles.module.css";
 
 export const Navigation = () => {
-	const { userData } = useSelector((state) => state.userData);
-	const [isSignedIn, setIsSignedIn] = useState(false);
-	const [isOpen, setIsOpen] = useState(false);
+    const { userData } = useSelector((state) => state.userData);
+    const [isSignedIn, setIsSignedIn] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-	useEffect(() => {
-		if (localStorage.getItem("sb-ginityejycllnrdlmmue-auth-token")) {
-			setIsSignedIn(true);
-		} else {
-			setIsSignedIn(false);
-		}
-	}, [userData]);
+    useEffect(() => {
+        if (localStorage.getItem("sb-ginityejycllnrdlmmue-auth-token")) {
+            setIsSignedIn(true);
+        } else {
+            setIsSignedIn(false);
+        }
+    }, [userData]);
 
-	return (
-		<header className="relative z-10 flex flex-row items-center justify-between bg-[#09080D] bg-opacity-50 px-20 py-7 text-center max-[600px]:px-5 max-[600px]:py-5 min-[600px]:backdrop-blur-sm">
-			<Logo />
-			<nav className={`${NavigationStyles.nav}`} data-isopen={isOpen}>
-				<div className="min-[600px]:hidden">
-					<UserAccessGroup isSignedIn={isSignedIn} />
-				</div>
-				<ul
-					className={`flex text-[1.2rem] text-primary min-[600px]:ml-20 ${NavigationStyles.nav_list}`}
-				>
-					<li>
-						<NavLink to="/" className={NavigationStyles.nav_link}>
-							Home
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/create" className={NavigationStyles.nav_link}>
-							Create
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/campaigns" className={NavigationStyles.nav_link}>
-							Campaigns
-						</NavLink>
-					</li>
-				</ul>
-				
-			</nav>
-			<div className="flex gap-5">
-				<MenuAndProfileGroup
-					isOpen={isOpen}
-					setIsOpen={setIsOpen}
-					isSignedIn={isSignedIn}
-				/>
-				<div className={`max-[600px]:hidden`}>
-					<UserAccessGroup isSignedIn={isSignedIn} />
-				</div>
-			</div>
-		</header>
-	);
+    return (
+        <header className="relative z-10 flex flex-row items-center justify-between bg-[#09080D] bg-opacity-50 px-20 py-7 text-center max-[600px]:px-5 max-[600px]:py-5 min-[600px]:backdrop-blur-sm">
+            <Logo />
+            <nav className={`${NavigationStyles.nav}`} data-isopen={isOpen}>
+                <div className="min-[600px]:hidden">
+                    <UserAccessGroup isSignedIn={isSignedIn} />
+                </div>
+                <ul
+                    className={`flex text-[1.2rem] text-primary min-[600px]:ml-20 ${NavigationStyles.nav_list}`}
+                >
+                    <li>
+                        <NavLink to="/" className={NavigationStyles.nav_link}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/create"
+                            className={NavigationStyles.nav_link}
+                        >
+                            Create
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/campaigns"
+                            className={NavigationStyles.nav_link}
+                        >
+                            Campaigns
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <div className="flex gap-5">
+                <MenuAndProfileGroup
+                    isOpen={isOpen}
+                    setIsOpen={setIsOpen}
+                    isSignedIn={isSignedIn}
+                />
+                <div className={`max-[600px]:hidden`}>
+                    <UserAccessGroup isSignedIn={isSignedIn} />
+                </div>
+            </div>
+        </header>
+    );
 };
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect } from "react";
-import { Header } from "../components/Home.jsx/Header";
 import { motion } from "framer-motion";
 import { useOutletContext } from "react-router-dom";
+import { NavLink } from "react-router-dom"
+;
+import { useSelector } from "react-redux";
 import { pageChange } from "../redux/page";
+
+
+
 
 export const Home = () => {
 	const {dispatch} = useOutletContext();
+	const { userData } = useSelector((state) => state.userData);
+
 
 	useEffect(() => {
 		dispatch(pageChange("home"));
@@ -13,7 +20,21 @@ export const Home = () => {
 
 	return (
 		<motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="flex flex-col items-start justify-between pb-32 pt-0 max-[600px]:items-center">
-			<Header />
+			<div className="flex flex-col gap-10 max-[600px]:text-center">
+				<h1 className="text-7xl text-primary max-[600px]:text-5xl">
+					All you have to do is ROLL!
+				</h1>
+				<p className="text-2xl text-neutral max-[600px]:text-[1rem]">
+					Choose from a plethora of customization options. Make your own campaigns and
+					feature <br /> them for everyone to see!
+				</p>
+			</div>
+			<NavLink
+				to={userData ? "/create" : "/signin"}
+				className="btn btn-primary font-semibold text-neutral-content hover:scale-[1.1] hover:bg-primary"
+			>
+				Get Started
+			</NavLink>
 		</motion.section>
 	);
 };
