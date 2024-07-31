594f601b99159265a4f331563a67ced8cd83e448
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-07-31 21:53:53 +0200

    Add update campaign description feature

--- src/components/Campaign/Campaign.jsx
+++ src/components/Campaign/Campaign.jsx
@@ -6,40 +6,23 @@ import { supabase } from "../../supabase/supabase";
 import { useSelector } from "react-redux";
 
 import { InteractiveMap } from "../InteractiveMap/InteractiveMap";
-import Markdown from "markdown-to-jsx";
+import { CampaignDescription } from "./CampaignDescription";
 
 export const Campaign = () => {
     const [mapUrl, setMapUrl] = useState("");
-    const [description, setDescription] = useState("");
     const [isLoaded, setIsLoaded] = useState(false);
-    const [updatedDescription, setUpdatedDescription] = useState();
 
     const { state } = useLocation();
 
     const { userData } = useSelector((state) => state.userData);
     const navigate = useNavigate();
 
+
     const getImageFromStorage = async () => {
         const { data } = supabase.storage
             .from("maps")
             .getPublicUrl(state.meta_data.map);
         setMapUrl(data.publicUrl);
-    };
-    const getDescriptionFromStorage = async () => {
-        const { data } = supabase.storage
-            .from("descriptions")
-            .getPublicUrl(state.meta_data.description);
-        const response = await fetch(`${data.publicUrl}.md`);
-        setDescription(await response.text());
-    };
-
-    const updateDescription = async (e) => {
-        const { data, error } = await supabase.storage
-            .from("descriptions")
-            .update(`${state.meta_data.description}.md`, updatedDescription, {
-                cacheControl: "3600",
-                upsert: true,
-            });
     };
 
     useEffect(() => {
@@ -51,12 +34,19 @@ export const Campaign = () => {
         getImageFromStorage();
         window.scrollTo(0, 0);
     }, []);
+    
 
-	useEffect(() => {
-		getDescriptionFromStorage();
-	}, [updatedDescription])
+    if (!state?.id) return (
+        <motion.section
+            className="flex h-full w-full flex-col justify-center gap-20"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+        >
+            <h2 className="text-5xl text-warning">Nothing to see here!</h2>
+        </motion.section>
 
-    if (!state?.id) return <h1>This campaign doesn't exist!</h1>;
+        
+    )
 
     return (
         <motion.section
@@ -80,61 +70,7 @@ export const Campaign = () => {
             </div>
 
             <div className="rounded-lg bg-black bg-opacity-70 px-10 py-5 text-xl">
-                {userData.user.id === state.profile && (
-                    <div className="w-full px-10 py-5">
-                        <button
-                            className="btn btn-ghost"
-                            onClick={() => window.my_modal_1.showModal()}
-                        >
-                            Upload edit
-                        </button>
-                        <dialog id="my_modal_1" className="modal">
-                            <form
-                                method="dialog"
-                                className="modal-box"
-                                onSubmit={(e) => updateDescription(e)}
-                            >
-                                <div className="flex flex-col gap-2">
-                                    <label htmlFor="map">
-                                        Upload your updated description (.md):
-                                    </label>
-                                    <input
-                                        type="file"
-                                        id="description"
-                                        onChange={(e) =>
-                                            setUpdatedDescription(
-                                                e.target.files[0]
-                                            )
-                                        }
-                                    />
-                                </div>
-                                <button type="submit">Update</button>
-                            </form>
-                        </dialog>
-                    </div>
-                )}
-                <Markdown
-                    options={{
-                        overrides: {
-                            h1: {
-                                component: (props) => (
-                                    <h1 className="mb-10 text-5xl font-bold text-secondary max-[700px]:text-4xl">
-                                        {props.children}
-                                    </h1>
-                                ),
-                            },
-                            p: {
-                                component: (props) => (
-                                    <p className="py-6 max-[700px]:text-[.9rem]">
-                                        {props.children}
-                                    </p>
-                                ),
-                            },
-                        },
-                    }}
-                >
-                    {description}
-                </Markdown>
+                <CampaignDescription state={state}/>
             </div>
         </motion.section>
     );
---
+++ src/components/Campaign/CampaignDescription.jsx
@@ -0,0 +1,103 @@
+import Markdown from "markdown-to-jsx";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../../supabase/supabase";
+import { useSelector } from "react-redux";
+
+export const CampaignDescription = ({ state }) => {
+    const [desc, setDesc] = useState("");
+    const [updatedDesc, setUpdatedDesc] = useState({});
+    const { userData } = useSelector((state) => state.userData);
+    
+    // const Foo = async () => {
+    //     setDesc(await updatedDesc.text())
+    // }
+
+
+    const updateDescription = async () => {
+        const parsedDesc = await updatedDesc.text();
+        const { data, error } = await supabase
+            .from("campaigns")
+            .update({ campaign_description:  parsedDesc})
+            .eq("id", state.id);
+            if (error) {
+                console.error(error);
+                return;
+            }
+            setDesc(parsedDesc);
+    };
+
+    
+
+    useEffect(() => {
+        (async () => {
+            const { data, error } = await supabase 
+                .from("campaigns")
+                .select("campaign_description")
+                .eq("id", state.id)
+                .single();
+            if (error) {
+                console.error(error);
+                return;
+            }
+            setDesc(data.campaign_description);
+        })();
+    }, [updatedDesc]);
+
+
+    return (
+        <>
+            {userData.user.id === state.profile && (
+                <div className="w-full px-10 py-5">
+                    <button
+                        className="btn btn-ghost"
+                        onClick={() => window.my_modal_1.showModal()}
+                    >
+                        Upload edit
+                    </button>
+                    <dialog id="my_modal_1" className="modal">
+                        <form
+                            method="dialog"
+                            className="modal-box"
+                            onSubmit={updateDescription}
+                        >
+                            <div className="flex flex-col gap-2">
+                                <label htmlFor="description">
+                                    Upload your updated description (*.md):
+                                </label>
+                                <input
+                                    type="file"
+                                    id="description"
+                                    name="description"
+                                    onChange={(e) => setUpdatedDesc(e.target.files[0])}
+                                />
+                            </div>
+                            <button type="submit">Update</button>
+                        </form>
+                    </dialog>
+                </div>
+            )}
+            <Markdown
+                options={{
+                    overrides: {
+                        h1: {
+                            component: (props) => (
+                                <h1 className="mb-10 text-5xl font-bold text-secondary max-[700px]:text-4xl">
+                                    {props.children}
+                                </h1>
+                            ),
+                        },
+                        p: {
+                            component: (props) => (
+                                <p className="py-6 max-[700px]:text-[.9rem]">
+                                    {props.children}
+                                </p>
+                            ),
+                        },
+                    },
+                }}
+            >
+                {desc}
+            </Markdown>
+        </>
+    );
+};
